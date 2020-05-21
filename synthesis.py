c76c485bf5d36117c7d339dbfdb9b2e86ff95da4
William Ehlhardt <williamehlhardt@gmail.com>
2020-05-20 19:30:02 -0700

    basic synthesis

--- synthesis.py
+++ synthesis.py
@@ -1,13 +1,14 @@
 #! /usr/bin/env python3
 
 import matplotlib.pyplot as plt
+import math
 import numpy as np
 import scipy
 import scipy.signal
 import scipy.io.wavfile
 import sys
 
-_, inp = sys.argv
+_, inp,outp = sys.argv
 
 rate, rawdata = scipy.io.wavfile.read(inp, mmap=True)
 print(rate, rawdata.shape)
@@ -20,12 +21,28 @@ freqs, wtimes, spectro = scipy.signal.spectrogram(data)
 
 #print(freqs, len(freqs))
 print(len(freqs))
-#print(wtimes)
+print(wtimes)
 #print(spectro)
 print(len(spectro), len(spectro[0]))
 
 for i,f in enumerate(freqs):
     print(i,f)
 
-plt.imshow(spectro, cmap='hot', interpolation='nearest')
-plt.show()
+def synthesize_block(width, frequencies, amplitudes):
+    samples=[]
+    for t in range(width):
+        sample = 0
+        for f,amp in zip(frequencies, amplitudes):
+            component = math.sin(t*f)*amp
+            sample += component
+        samples.append(sample)
+    return samples
+
+blocks = []
+for blockid, amplitudes in enumerate(np.transpose(spectro)):
+    blocks.append(synthesize_block(128, freqs, amplitudes))
+    print('synthesized block', blockid)
+samples = sum(blocks, [])
+samples = np.asarray(samples)
+
+scipy.io.wavfile.write(outp, rate, samples)
